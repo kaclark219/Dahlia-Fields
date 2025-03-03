88d036563061bdc7779983d63911050da4dfc7dd
Nandni Solanki <nsolanki@tamu.edu>
2025-03-03 15:35:27 -0600

    Added all flowers to Inventory

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -7,26 +7,21 @@ public class InventoryItem
 {
     public string itemName;
     public float seedPrice;
-    public float flowerValue;
     public string description;
     public int seedStock;
     public int flowerStock;
     public int daysToGrow;
-    public Sprite planted;
     public Sprite growing;
     public Sprite harvest;
 
-    public InventoryItem(string name, float cost, float value, string desc, int amtS, int amtF, int days, 
-        Sprite p, Sprite g, Sprite h)
+    public InventoryItem(string name, float cost, string desc, int amtS, int amtF, int days, Sprite g, Sprite h)
     {
         itemName = name;
         seedPrice = cost;
-        flowerValue = value;
         description = desc;
         seedStock = amtS;
         flowerStock = amtF;
         daysToGrow = days;
-        planted = p;
         growing = g;
         harvest = h;
     }
@@ -34,24 +29,64 @@ public class InventoryItem
 
 public class InventoryManager : MonoBehaviour
 {
-    [SerializeField] public GameObject dandelionPlanted;
-    [SerializeField] public GameObject dandelionGrowing;
-    [SerializeField] public GameObject dandelionHarvest;
+    [SerializeField] public Sprite dandelionGrowing;
+    [SerializeField] public Sprite dandelionHarvest;
 
-    [SerializeField] public GameObject daisyPlanted;
-    [SerializeField] public GameObject daisyGrowing;
-    [SerializeField] public GameObject daisyHarvest;
+    [SerializeField] public Sprite daisyGrowing;
+    [SerializeField] public Sprite daisyHarvest;
+
+    [SerializeField] public Sprite poppyGrowing;
+    [SerializeField] public Sprite poppyHarvest;
+
+    [SerializeField] public Sprite tulipGrowing;
+    [SerializeField] public Sprite tulipHarvest;
+
+    [SerializeField] public Sprite roseGrowing;
+    [SerializeField] public Sprite roseHarvest;
+
+    [SerializeField] public Sprite lavenderGrowing;
+    [SerializeField] public Sprite lavenderHarvest;
+
+    [SerializeField] public Sprite pearGrowing;
+    [SerializeField] public Sprite pearHarvest;
+
+    [SerializeField] public Sprite sunflowerGrowing;
+    [SerializeField] public Sprite sunflowerHarvest;
+
+    [SerializeField] public Sprite lilyGrowing;
+    [SerializeField] public Sprite lilyHarvest;
 
     public Dictionary<string, InventoryItem> inventory = new Dictionary<string, InventoryItem>();
 
     // Start is called before the first frame update
     void Start()
     {
-        inventory.Add("Dandelion", new InventoryItem("Dandelion", 12.0f, 5.0f, "Ava favorite", 10, 0, 2,
-            dandelionPlanted.GetComponent<SpriteRenderer>().sprite, dandelionGrowing.GetComponent<SpriteRenderer>().sprite, dandelionHarvest.GetComponent<SpriteRenderer>().sprite));
+        inventory.Add("Dandelion", new InventoryItem("Dandelion", 12.0f, "Ava favorite", 0, 0, 2,
+            dandelionGrowing, dandelionHarvest));
 
-        inventory.Add("Daisy", new InventoryItem("Daisy", 24.0f, 10.0f, "Bruce favorite", 5, 0, 3, 
-            daisyPlanted.GetComponent<SpriteRenderer>().sprite, daisyGrowing.GetComponent<SpriteRenderer>().sprite, daisyHarvest.GetComponent<SpriteRenderer>().sprite));
+        inventory.Add("Daisy", new InventoryItem("Daisy", 24.0f, "Bruce favorite", 0, 0, 3, 
+            daisyGrowing, daisyHarvest));
+
+        inventory.Add("Poppy", new InventoryItem("Poppy", 36.0f, "Linda favorite", 0, 0, 3,
+            poppyGrowing, poppyHarvest));
+
+        inventory.Add("Tulip", new InventoryItem("Tulip", 48.0f, "Megan favorite", 0, 0, 4,
+            tulipGrowing, tulipHarvest));
+
+        inventory.Add("Rose", new InventoryItem("Rose", 60.0f, "Charlie favorite", 0, 0, 4,
+            roseGrowing, roseHarvest));
+
+        inventory.Add("Lavender", new InventoryItem("Lavender", 72.0f, "Gerald favorite", 0, 0, 5,
+            lavenderGrowing, lavenderHarvest));
+
+        inventory.Add("PricklyPear", new InventoryItem("PricklyPear", 84.0f, "Jermey favorite", 0, 0, 6,
+            pearGrowing, pearHarvest));
+
+        inventory.Add("Sunflower", new InventoryItem("Sunflower", 96.0f, "Sebastian favorite", 0, 0, 7,
+            sunflowerGrowing, sunflowerHarvest));
+
+        inventory.Add("LilyValley", new InventoryItem("LilyValley", 108.0f, "Poppy favorite", 0, 0, 7,
+            lilyGrowing, lilyHarvest));
 
     }
 
