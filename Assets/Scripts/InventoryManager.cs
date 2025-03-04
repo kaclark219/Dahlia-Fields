9f6adc19da248286c363dfbc0afa7d098d2791b7
Nandni Solanki <nsolanki@tamu.edu>
2025-03-04 01:24:50 -0600

    Added UI Code for Inventory Slot Info

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -13,8 +13,10 @@ public class InventoryItem
     public int daysToGrow;
     public Sprite growing;
     public Sprite harvest;
+    public Sprite seedIcon;
+    public Sprite flowerIcon;
 
-    public InventoryItem(string name, float cost, string desc, int amtS, int amtF, int days, Sprite g, Sprite h)
+    public InventoryItem(string name, float cost, string desc, int amtS, int amtF, int days, Sprite g, Sprite h, Sprite s, Sprite f)
     {
         itemName = name;
         seedPrice = cost;
@@ -24,6 +26,8 @@ public class InventoryItem
         daysToGrow = days;
         growing = g;
         harvest = h;
+        seedIcon = s;
+        flowerIcon = f;
     }
 }
 
@@ -31,30 +35,48 @@ public class InventoryManager : MonoBehaviour
 {
     [SerializeField] public Sprite dandelionGrowing;
     [SerializeField] public Sprite dandelionHarvest;
+    [SerializeField] public Sprite dandelionSeed;
+    [SerializeField] public Sprite dandelionFlower;
 
     [SerializeField] public Sprite daisyGrowing;
     [SerializeField] public Sprite daisyHarvest;
+    [SerializeField] public Sprite daisySeed;
+    [SerializeField] public Sprite daisyFlower;
 
     [SerializeField] public Sprite poppyGrowing;
     [SerializeField] public Sprite poppyHarvest;
+    [SerializeField] public Sprite poppySeed;
+    [SerializeField] public Sprite poppyFlower;
 
     [SerializeField] public Sprite tulipGrowing;
     [SerializeField] public Sprite tulipHarvest;
+    [SerializeField] public Sprite tuilpSeed;
+    [SerializeField] public Sprite tuilpFlower;
 
     [SerializeField] public Sprite roseGrowing;
     [SerializeField] public Sprite roseHarvest;
+    [SerializeField] public Sprite roseSeed;
+    [SerializeField] public Sprite roseFlower;
 
     [SerializeField] public Sprite lavenderGrowing;
     [SerializeField] public Sprite lavenderHarvest;
+    [SerializeField] public Sprite lavenderSeed;
+    [SerializeField] public Sprite lavenderFlower;
 
     [SerializeField] public Sprite pearGrowing;
     [SerializeField] public Sprite pearHarvest;
+    [SerializeField] public Sprite pearSeed;
+    [SerializeField] public Sprite pearFlower;
 
     [SerializeField] public Sprite sunflowerGrowing;
     [SerializeField] public Sprite sunflowerHarvest;
+    [SerializeField] public Sprite sunflowerSeed;
+    [SerializeField] public Sprite sunflowerFlower;
 
     [SerializeField] public Sprite lilyGrowing;
     [SerializeField] public Sprite lilyHarvest;
+    [SerializeField] public Sprite lilySeed;
+    [SerializeField] public Sprite lilyFlower;
 
     public Dictionary<string, InventoryItem> inventory = new Dictionary<string, InventoryItem>();
 
@@ -62,31 +84,31 @@ public class InventoryManager : MonoBehaviour
     void Start()
     {
         inventory.Add("Dandelion", new InventoryItem("Dandelion", 12.0f, "Ava favorite", 0, 0, 2,
-            dandelionGrowing, dandelionHarvest));
+            dandelionGrowing, dandelionHarvest, dandelionSeed, dandelionFlower));
 
         inventory.Add("Daisy", new InventoryItem("Daisy", 24.0f, "Bruce favorite", 0, 0, 3, 
-            daisyGrowing, daisyHarvest));
+            daisyGrowing, daisyHarvest, daisySeed, daisyFlower));
 
         inventory.Add("Poppy", new InventoryItem("Poppy", 36.0f, "Linda favorite", 0, 0, 3,
-            poppyGrowing, poppyHarvest));
+            poppyGrowing, poppyHarvest, poppySeed, poppyFlower));
 
         inventory.Add("Tulip", new InventoryItem("Tulip", 48.0f, "Megan favorite", 0, 0, 4,
-            tulipGrowing, tulipHarvest));
+            tulipGrowing, tulipHarvest, tuilpSeed, tuilpFlower));
 
         inventory.Add("Rose", new InventoryItem("Rose", 60.0f, "Charlie favorite", 0, 0, 4,
-            roseGrowing, roseHarvest));
+            roseGrowing, roseHarvest, roseSeed, roseFlower));
 
         inventory.Add("Lavender", new InventoryItem("Lavender", 72.0f, "Gerald favorite", 0, 0, 5,
-            lavenderGrowing, lavenderHarvest));
+            lavenderGrowing, lavenderHarvest, lavenderSeed, lavenderFlower));
 
         inventory.Add("PricklyPear", new InventoryItem("PricklyPear", 84.0f, "Jermey favorite", 0, 0, 6,
-            pearGrowing, pearHarvest));
+            pearGrowing, pearHarvest, pearSeed, pearFlower));
 
         inventory.Add("Sunflower", new InventoryItem("Sunflower", 96.0f, "Sebastian favorite", 0, 0, 7,
-            sunflowerGrowing, sunflowerHarvest));
+            sunflowerGrowing, sunflowerHarvest, sunflowerSeed, sunflowerFlower));
 
         inventory.Add("LilyValley", new InventoryItem("LilyValley", 108.0f, "Poppy favorite", 0, 0, 7,
-            lilyGrowing, lilyHarvest));
+            lilyGrowing, lilyHarvest, lilySeed, lilyFlower));
 
     }
 
--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryUI : InteractableObj
 {
     [SerializedField] public Canvas uiScreen;
     [SerializedField] public InventoryManager inventoryManager;
+    private InventoryItem item; 
     public bool flowers = false;
-    public bool seeds = false; 
+    public bool seeds = false;
+
+    //view elements
+    [SerializedField] public GameObject view; 
+    [SerializedField] public TextMeshProUGUI viewName;
+    [SerializedField] public TextMeshProUGUI viewStock;
+    [SerializedField] public TextMeshProUGUI viewDescription;
+    [SerializedField] public GameObject viewIcon;
+
+    //slots
+    [SerializedField] public Image[] iconSlots = new Image[9];
+    [SerializedField] public TextMeshProUGUI[] amounts = new TextMeshProUGUI[9];
 
     public override void Start()
     {
         base.Start();
+        view.SetActive(false);
         uiScreen.enabled = false;
+        
 
     }
 
@@ -26,16 +44,33 @@ public class InventoryUI : InteractableObj
     {
         seeds = false;
         flowers = true;
+        view.SetActive(false);
+        int count = 0; 
+        foreach (var item in inventoryManager.inventory)
+        {
+            iconSlots[count].sprite = item.Value.flowerIcon;
+            amounts[count].text = item.Value.flowerStock.ToString();
+            count++;
+        }
     }
 
     public void SetUpSeeds()
     {   
         flowers = false;
         seeds = true;
+        view.SetActive(false);
+        int count = 0;
+        foreach (var item in inventoryManager.inventory)
+        {
+            iconSlots[count].sprite = item.Value.seedIcon;
+            amounts[count].text = item.Value.seedStock.ToString();
+            count++;
+        }
     }
 
     public void SetUpView(string name)
     {
+        view.SetActive(true); 
         if(flowers)
         {
 
