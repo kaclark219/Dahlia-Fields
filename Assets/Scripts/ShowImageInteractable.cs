5b90a8e0a5551682bc99676112c99f64b8910d38
Nandni Solanki <nsolanki@tamu.edu>
2025-04-14 21:06:11 -0500

    Added SFX to Tutorial Notes

--- Assets/Scripts/ShowImageInteractable.cs
+++ Assets/Scripts/ShowImageInteractable.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class ShowImageInteractable : InteractableObj
 {
     [SerializeField] private GameObject UI;
+    private SoundEffects effect;
+
+    public override void Awake()
+    {
+        base.Awake();
+        effect = GameObject.Find("SoundEffectManager").GetComponent<SoundEffects>();
+
+    }
 
     public override void Start()
     {
@@ -26,12 +34,14 @@ public class ShowImageInteractable : InteractableObj
 
     public override void OnInteract()
     {
+        effect.PlayMail(); 
         base.OnInteract();
         UI.SetActive(true);
     }
 
     public override void EndInteract()
-    {
+    {   
+        effect.PlayClose();
         base.EndInteract();
         UI.SetActive(false);
     }
